cc4b56a399324a7671fc96bdcbde245749e7c3cc
NyIIIa <83402742+NyIIIa@users.noreply.github.com>
2023-07-04 19:42:49 +0300

    added connection string

--- TechTask.WebApi/Program.cs
+++ TechTask.WebApi/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TechTask.WebApi.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
