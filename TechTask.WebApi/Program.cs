70f3d2bac04c420052daa1e5ecf73f675d507e52
NyIIIa <83402742+NyIIIa@users.noreply.github.com>
2023-07-05 17:07:20 +0300

    added mediatr to DI

--- TechTask.WebApi/Program.cs
+++ TechTask.WebApi/Program.cs
@@ -1,4 +1,7 @@
+using System.Globalization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 using TechTask.WebApi.Infrastructure.Interfaces;
 using TechTask.WebApi.Infrastructure.Services;
 using TechTask.WebApi.Persistence;
@@ -9,11 +12,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IUserEstateManager, UserEstateManager>();
 builder.Services.AddDbContext<ApplicationDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.MapType<DateTime>(() => new OpenApiSchema
+    {
+        Type = "string",
+        Format = "date-time"
+    });
+});
 
 var app = builder.Build();
 
@@ -23,9 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
-
 app.MapControllers();
 
 app.Run();
